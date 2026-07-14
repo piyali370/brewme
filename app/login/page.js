1f813f7ab7d9b9927efc7fb5be58f27edd1dfcd2
PIYALI <piyaofficial05@gmail.com>
2026-07-14 13:45:31 +0530

    Remove GitHub authentication

--- app/login/page.js
+++ app/login/page.js
@@ -109,7 +109,7 @@ const Login = () => {
 
 
 
-                <button onClick={() => { signIn("github") }}
+                {/* <button onClick={() => { signIn("github") }}
                     className="flex items-center w-64 bg-white bg-[radial-gradient(60%_120%_at_50%_50%,hsla(0,0%,100%,0)_0,rgba(252,205,238,.5)_100%)] border border-gray-300 rounded-lg shadow-md max-w-xs px-6 py-2 text-sm font-medium text-gray-800 hover:bg-gray-200 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-gray-500">
                     <svg className="h-6 w-6 mr-2" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink"
                         viewBox="0 0 73 73" version="1.1">
@@ -130,7 +130,7 @@ const Login = () => {
                     </svg>
 
                     <span>Continue with Github</span>
-                </button>
+                </button> */}
 
 
 
